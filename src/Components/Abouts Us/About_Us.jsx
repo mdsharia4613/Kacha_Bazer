d865217e4eee2efac558da1d1c451d107121043f
mdsharia4613 <mdsharia4613@gmail.com>
2025-11-06 03:16:38 +0600

    about

--- src/Components/Abouts Us/About_Us.jsx
+++ src/Components/Abouts Us/About_Us.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
 
+import bammer from '../../assets/about/banner.jpg';
 const About_Us = () => {
     return (
-        <div>
-            <h2>hello</h2>
+       <>
+            <div className="relative w-full">
+                <img
+                    src={bammer}
+                    alt="About Banner"
+                    className="w-full h-[300px] object-cover brightness-90"
+                />
+                <h2 className="absolute inset-0 flex items-center justify-center  text-4xl md:text-5xl font-bold z-10">
+                    About Us
+                </h2>
+            </div>
+        <div className="container mx-auto my-10 p-5">
+            
         </div>
+       </>
     );
 };
 
