c283c9597e9b6a4d8c9eeb5413b330101be3f403
mdsharia4613 <mdsharia4613@gmail.com>
2025-11-07 04:04:21 +0600

    shopping icon change

--- src/Components/CategoryProducts/CategoryProducts.jsx
+++ src/Components/CategoryProducts/CategoryProducts.jsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from "react";
 import productData from "../../../public/category_data.json";
 import { useOutletContext, useParams } from "react-router-dom";
+import { FaRegStar, FaStar, FaStarHalf } from "react-icons/fa6";
+import { FaShoppingBag } from "react-icons/fa";
 
 const CategoryProducts = () => {
     const {categoryName} = useParams();
@@ -24,40 +26,65 @@ const CategoryProducts = () => {
     }, [categoryName]);
     const {handleAddToCart} = useOutletContext();
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto mt-10">
             <h2 className="font-semibold text-2xl">{categoryName}</h2>
             <div>
                 {items.length === 0 ? (
                     <p className="text-center text-gray-500">No products found in this category.</p>
                 ) : (
-                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 place-items-center">
-                        {items.map(item => (
-                            <div key={item.id} className="border border-gray-200 rounded-xl p-4 shadow hover:shadow-lg transition-all duration-300 bg-white w-72">
-                                <img
-                                    className="w-full h-52 object-contain rounded-lg mb-3"
-                                    src={item.image}
-                                    alt={item.name}
-                                />
-                                <h3 className="text-lg font-semibold text-gray-700 mb-1">
-                                    {item.name}
-                                </h3>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 place-items-center mt-8">
+                        {items.map(item => {
 
-                                <p className="text-lg font-bold text-cyan-600">${item.price}</p>
-                                <p
-                                    className={`text-sm mt-1 ${item.stockStatus === "In Stock"
-                                        ? "text-green-500"
-                                        : "text-red-500"
-                                        }`}
-                                >
-                                    {item.stockStatus}
-                                </p>
+                            const stars = item.rating;
+                            const fullStars = Math.floor(stars);
+                            const halStar = stars % 1 >= 0.5;
+                            return (
+                                <div key={item.id} className="border border-gray-200 rounded-xl p-4 shadow hover:shadow-lg transition-all duration-300 bg-white w-72">
+                                    <img
+                                        className="w-full h-52 object-contain rounded-lg mb-3"
+                                        src={item.image}
+                                        alt={item.name}
+                                    />
+                                    <h3 className="text-lg font-semibold text-gray-700 mb-1">
+                                        {item.name}
+                                    </h3>
 
-                                <button onClick={() => handleAddToCart(item)} className="mt-4 bg-[#00bc7d] text-white w-full py-2 rounded-md hover:bg-[#00a86b] transition duration-200">
-                                    Add to Cart
-                                </button>
+                                    <p className="text-lg font-bold text-cyan-600">${item.price}</p>
+                                    <p
+                                        className={`text-sm mt-1 ${item.stockStatus === "In Stock"
+                                            ? "text-green-500"
+                                            : "text-red-500"
+                                            }`}
+                                    >
+                                        {item.stockStatus}
+                                    </p>
 
-                            </div>
-                        ))}
+                                    <div className="flex justify-between items-center">
+                                        <button className="" onClick={() => handleAddToCart(item)} className="mt-4 bg-[#00bc7d] text-white  py-2 px-2 rounded-full hover:bg-[#00a86b] transition duration-200">
+                                            <FaShoppingBag fontSize="20" />
+                                        </button>
+                                        <div className="flex items-center gap-2">
+                                            <div className="flex ">
+                                                {Array.from({ length: 5 }, (_, i) => {
+                                                    if (i < fullStars) {
+                                                        return <FaStar key={i} className="text-yellow-400"></FaStar>
+                                                    }
+                                                    else if (i === fullStars && halStar) {
+                                                        return <FaStarHalf key={i} className="text-yellow-400"></FaStarHalf>
+                                                    }
+                                                    else {
+                                                        return <FaRegStar key={i} className="text-yellow-400"></FaRegStar>
+                                                    }
+
+                                                })}
+                                            </div>
+                                            <span>{item.rating}</span>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            )
+                        })}
                     </div>
                 )}
             </div>
--- src/Components/TopRating/TopRating.jsx
+++ src/Components/TopRating/TopRating.jsx
@@ -1,6 +1,10 @@
 import { useOutletContext } from 'react-router-dom';
 import productData from '../../../public/category_data.json';
 import { useState } from 'react';
+import { FaRegStar, FaStar, FaStarHalf } from "react-icons/fa6";
+import { FaShoppingBag } from "react-icons/fa";
+
+
 
 const TopRating = () => {
     const { handleAddToCart } = useOutletContext();
@@ -11,7 +15,7 @@ const TopRating = () => {
     const topRating = allItems.filter(item => item.rating > 4.5);
 
     const [visibleCount, setVisibleCount] = useState(12);
-    console.log('visibleCount' ,visibleCount);
+    console.log('visibleCount', visibleCount);
 
     const handleShowMore = () => {
         if (visibleCount < topRating.length) {
@@ -35,40 +39,62 @@ const TopRating = () => {
 
             {/* Product Cards */}
             <div className="flex flex-wrap justify-center gap-8">
-                {topRating.slice(0, visibleCount).map(item => (
-                    <div
-                        key={item.id}
-                        className="border relative border-gray-200 rounded-xl p-4 shadow hover:shadow-lg transition-all duration-300 bg-white w-72"
-                    >
-                        <img
-                            className="w-full h-44 pt-2 object-contain rounded-lg mb-3"
-                            src={item.image}
-                            alt={item.name}
-                        />
-                        <h3 className="text-lg font-semibold text-gray-700 mb-1">
-                            {item.name}
-                        </h3>
+                {topRating.slice(0, visibleCount).map(item => {
 
-                        <p className="text-lg font-bold text-cyan-600">৳{item.price}</p>
-                        <p className="text-gray-500 text-sm">Rating: ⭐{item.rating}</p>
+                    const stars = item.rating;
+                    const fullStars = Math.floor(stars);
+                    const halStar = stars % 1 >= 0.5;
+                    return (
+                        <div
+                            key={item.id}
+                            className="border relative border-gray-200 rounded-xl p-4 shadow hover:shadow-lg transition-all duration-300 bg-white w-72"
+                        >
+                            <img
+                                className="w-full h-44 pt-2 object-contain rounded-lg mb-3"
+                                src={item.image}
+                                alt={item.name}
+                            />
+                            <h3 className="text-lg font-semibold text-gray-700 mb-1">
+                                {item.name}
+                            </h3>
 
-                        <p
-                            className={`absolute -top-1 text-xs font-medium mt-1 text-white px-0.5 py-0.5 left-0 rounded-tl-xl rounded-br-xl ${item.stockStatus === 'In Stock'
+                            <p className="text-lg font-bold text-cyan-600">৳{item.price}</p>
+                            <p className="text-gray-500 text-sm">Rating: ⭐{item.rating}</p>
+
+                            <p
+                                className={`absolute -top-1 text-xs font-medium mt-1 text-white px-0.5 py-0.5 left-0 rounded-tl-xl rounded-br-xl ${item.stockStatus === 'In Stock'
                                     ? 'bg-green-500'
                                     : 'bg-red-500'
-                                }`}
-                        >
-                            {item.stockStatus}
-                        </p>
+                                    }`}
+                            >
+                                {item.stockStatus}
+                            </p>
 
-                        <button
-                            onClick={() => handleAddToCart(item)}
-                            className="mt-4 bg-[#00bc7d] text-white w-full py-2 rounded-md hover:bg-[#00a86b] transition duration-200"
-                        >
-                            Add to Cart
-                        </button>
-                    </div>
-                ))}
+                            <div className="flex justify-between items-center">
+                                <button className="" onClick={() => handleAddToCart(item)} className="mt-4 bg-[#00bc7d] text-white  py-2 px-2 rounded-full hover:bg-[#00a86b] transition duration-200">
+                                    <FaShoppingBag fontSize="20" />
+                                </button>
+                                <div className="flex items-center gap-2">
+                                    <div className="flex ">
+                                        {Array.from({ length: 5 }, (_, i) => {
+                                            if (i < fullStars) {
+                                                return <FaStar key={i} className="text-yellow-400"></FaStar>
+                                            }
+                                            else if (i === fullStars && halStar) {
+                                                return <FaStarHalf key={i} className="text-yellow-400"></FaStarHalf>
+                                            }
+                                            else {
+                                                return <FaRegStar key={i} className="text-yellow-400"></FaRegStar>
+                                            }
+
+                                        })}
+                                    </div>
+                                    <span>{item.rating}</span>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
 
             {/* Show More / Show Less Button */}
