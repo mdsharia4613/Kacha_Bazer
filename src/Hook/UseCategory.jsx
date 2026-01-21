ebe0838c5cf59646a15d0dd10de09ce3787356a4
mdsharia4613 <mdsharia4613@gmail.com>
2026-01-21 20:22:24 +0600

    api intregated

--- src/Hook/UseCategory.jsx
+++ src/Hook/UseCategory.jsx
@@ -4,7 +4,8 @@ const useCategory = () => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch("/category_data.json") 
+        // fetch("/category_data.json") 
+        fetch("https://mocki.io/v1/2084e3df-f98a-48d8-9a2a-fa1d0718cbc6") 
             .then((res) => res.json())
             .then((data) => setData(data));
     }, []);
